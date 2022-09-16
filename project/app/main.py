acd28710b52df92d863db55354dd4c8874d6d9c0
ugaemi <u.gaemi@gmail.com>
2022-09-16 20:15:17 +0900

    :sparkles: 콜렉터 북북이 마무리

--- project/app/main.py
+++ project/app/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
+from app.book_scraper import NaverBookScraper
 from app.models import mongodb
 from app.models.book import BookModel
 
@@ -15,16 +16,37 @@ templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    book = BookModel(keyword="파이썬", publisher="BJPublic", price=12000, image="me.png")
-    print(await mongodb.engine.save(book))
     return templates.TemplateResponse("./index.html", {"request": request, "title": "콜렉터 북북이"})
 
 
 @app.get("/search", response_class=HTMLResponse)
 async def search(request: Request, q: str):
+    if not q:
+        return templates.TemplateResponse(
+            "./index.html",
+            {"request": request},
+        )
+    if await mongodb.engine.find_one(BookModel, BookModel.keyword == q):
+        books = await mongodb.engine.find(BookModel, BookModel.keyword == q)
+        return templates.TemplateResponse(
+            "./index.html",
+            {"request": request, "keyword": q, "books": books},
+        )
+    naver_book_scraper = NaverBookScraper()
+    books = await naver_book_scraper.search(q, 10)
+    book_models = []
+    for book in books:
+        book_model = BookModel(
+            keyword=q,
+            publisher=book["publisher"],
+            discount=book["discount"],
+            image=book["image"],
+        )
+        book_models.append(book_model)
+    await mongodb.engine.save_all(book_models)
     return templates.TemplateResponse(
         "./index.html",
-        {"request": request, "title": "콜렉터 북북이", "keyword": q},
+        {"request": request, "keyword": q, "books": books},
     )
 
 
